cefc32185b3dedae2c75f34cf8cf567361f5746a
RafaelM <rafael.mattos09@gmail.com>
2023-04-19 20:23:28 -0300

    montando_projeto

---
+++ src/factory/ConnectionFactory.java
@@ -0,0 +1,28 @@
+package factory;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public DataSource dataSource;
+
+    public ConnectionFactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("djmael123");
+
+        this.dataSource = comboPooledDataSource;
+    }
+
+    public Connection recuperaConexao() {
+        try {
+            return this.dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
